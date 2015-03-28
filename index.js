f4ce3ac6e4b0221f48d49e2eafdd7ee8f064847b
alt-f13 <galyamin.d.d@gmail.com>
2015-03-28 22:59:45 +0000

    socket.io

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ var piblaster = require('pi-blaster.js');
 var path = require('path');
 var app = express();
 
-var RED_GPIO_PIN = 17;
+var RED_GPIO_PIN = 23;
 var GREEN_GPIO_PIN = 18;
-var BLUE_GPIO_PIN = 22;
+var BLUE_GPIO_PIN = 24;
+var current='000';
 
 
 //Serve public content - basically any file in the public folder will be available on the server.
@@ -39,21 +40,33 @@ app.get('/green/:value', function (req, res) {
     }
 });
 
-app.get('/blue/:value', function (req, res) {
-    console.log("blue = " + req.params.value);
-    var blueValue = req.params.value;
-    if( !isNaN( parseInt(blueValue) ) ){
-        piblaster.setPwm(BLUE_GPIO_PIN, blueValue/255);
+app.get('/current/', function (req, res) {
+    console.log("current = " + current);
+    res.send(current);
+});
+
+app.get('/rgb/:value', function (req, res) {
+    console.log("hex = " + req.params.value);
+    current=req.params.value;
+    var rgbValue = req.params.value;
+    if( !isNaN(rgbValue = parseInt(req.params.value, 16) ) ){
+        var r = (rgbValue >> 16) & 255;
+        var g = (rgbValue >> 8) & 255;
+        var b = rgbValue & 255;
+    console.log("rgb = " + r + "g:" + g + "b:" + b);
+        piblaster.setPwm(GREEN_GPIO_PIN, g/255);
+        piblaster.setPwm(BLUE_GPIO_PIN, b/255);
+        piblaster.setPwm(RED_GPIO_PIN, r/255);
         res.send('ok');
     } else {
         res.status(400).send('error');
     }
 });
 
+
 // Start listening on port 3000.
-var server = app.listen(3000, function () {
+var server = app.listen(3100, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('RGB LED Slider listening at http://%s:%s', host, port);
 });
-
